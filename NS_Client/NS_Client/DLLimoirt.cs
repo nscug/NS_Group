43339388b0989b8e84c2ab82d3c9cb55a948f1c1
Cute-abacus <34331076+Cute-abacus@users.noreply.github.com>
2019-05-12 10:38:31 +0800

    上传原项目

--- NS_Client/NS_Client/DLLimoirt.cs
+++ NS_Client/NS_Client/DLLimoirt.cs
@@ -10,14 +10,14 @@ namespace NS_Client
 {
     class DLLimport
     {
-        [DllImport(@"T:\学习\学习大三下\NS_Client\NS_Client\bin\Debug\testdl.dll", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(@"testdl.dll", CallingConvention = CallingConvention.Cdecl)]
         //下面是导入的dll的name
         public static extern int Authentication(string ID, string password);
 
-        [DllImport(@"T:\学习\学习大三下\NS_Client\NS_Client\bin\Debug\testdl.dll", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(@"testdl.dll", CallingConvention = CallingConvention.Cdecl)]
         public static extern int Search(string ID,int socket);
 
-        [DllImport(@"T:\学习\学习大三下\NS_Client\NS_Client\bin\Debug\testdl.dll", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(@"testdl.dll", CallingConvention = CallingConvention.Cdecl)]
         public static extern int test(string a);
     }
 }
